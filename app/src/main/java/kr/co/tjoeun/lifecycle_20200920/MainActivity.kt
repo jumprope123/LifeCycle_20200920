7a0d7b18a4e5c7976e5cd0ef5f1f2b34c1cdd2a1
JiHwan Shin <jumprope123@naver.com>
2020-09-20 15:35:59 +0900

    onCrate에 대한 실험

--- app/src/main/java/kr/co/tjoeun/lifecycle_20200920/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20200920/MainActivity.kt
@@ -1,11 +1,22 @@
 package kr.co.tjoeun.lifecycle_20200920
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+        }
+
+        Log.d("메인 화면", "onCreate 실행됨")
     }
 }
---
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20200920/OtherActivity.kt
@@ -0,0 +1,19 @@
+package kr.co.tjoeun.lifecycle_20200920
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_other.*
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+
+        Log.d("다른화면","onCreate 실행됨")
+
+        goBackBtn.setOnClickListener {
+            finish()
+        }
+    }
+}
